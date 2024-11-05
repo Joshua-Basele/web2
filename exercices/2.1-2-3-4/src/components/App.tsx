3e312ffdd35d557b2da3a64d25bc7678b2a3b734
Joshua <joshua.basele@student.vinci.be>
2024-11-05 19:55:22 +0100

    new : 2.4 bonus

--- exercices/2.1-2-3-4/src/components/App.tsx
+++ exercices/2.1-2-3-4/src/components/App.tsx
@@ -1,6 +1,8 @@
 import Cinema from "./Cinema";
 import PageTitle from "./PageTitle";
-
+import Header from "./Header";
+import Footer from "./Footer";
+import "./App.css";
 
 const App = () => {
   const pageTitle = "Informations sur les films dans les cinémas";
@@ -50,11 +52,20 @@ const moviesCinema2 = [
   
   return (
     <div>
+      <Header url="">
+        <p>TOUT LES FILMS</p>
+      </Header>
+
       <PageTitle title={pageTitle} />
 
-      <Cinema name={cinema1Name} movies= {moviesCinema1} />
+      <Cinema name={cinema1Name} movies= {moviesCinema1}/>
 
-      <Cinema name={cinema2Name} movies={moviesCinema2} />
+      <Cinema name={cinema2Name} movies={moviesCinema2}/>
+
+      <Footer url="https://plus.unsplash.com/premium_photo-1710366327112-1c6b77517ff4?q=80&w=2914&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+        <p>© 2024 UGC Cinémas</p>
+      </Footer>
+
     </div>
   );
 };
